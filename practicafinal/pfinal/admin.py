5bc81121663e297fa2448eb1caf0e9675a90c952
arubiopa <anggylotis@gmail.com>
2016-05-22 18:02:43 +0200

    elimino archivos

--- practicafinal/pfinal/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from models import Hotel,Imagen,Comentario,HotelSeleccionado,PagCSS
-# Register your models here.
-admin.site.register(Hotel)
-admin.site.register(Imagen)
-admin.site.register(Comentario)
-admin.site.register(HotelSeleccionado)
-admin.site.register(PagCSS)
